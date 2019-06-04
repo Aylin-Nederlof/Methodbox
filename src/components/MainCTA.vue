ad74898b3e61fa63c45ea7f9be44bc642bab4a95
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-04 13:12:23 +0200

    Add mainCTA button component

---
+++ src/components/MainCTA.vue
@@ -0,0 +1,36 @@
+<template>
+  <button>
+  <p><slot></slot></p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'MainCTA',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+button{
+  height: 48px;
+  color: white;
+  border-radius: 4px;
+  background-color: #1F2933;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  padding: 0;
+}
+
+p{
+  text-align: center;
+  margin: 12px 16px;
+  line-height: 24px;
+  align-self: middle;
+}
+</style>
