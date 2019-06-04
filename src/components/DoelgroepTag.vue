3d785499c90474bb87fffd7ea91f19db24b4d7bf
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-04 18:50:17 +0200

    Create DoelgroepTag.vue

---
+++ src/components/DoelgroepTag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="doelgroepTag">
+  <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'doelgroepTag',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.doelgroepTag {
+  display: flex;
+  height: 32px;
+  border-radius: 4px;
+  border: 1px solid rgba(228,232,235,1);
+  align-items: center;
+  text-align: center;
+  padding: 4px 16px;
+  margin: 8px 8px 0 0;
+  font-size: 16px;
+  line-height: 1.5;
+
+}
+
+</style>
