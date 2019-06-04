7ea84112ccab79b35d9f2383e311528da61cb4af
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-04 15:25:57 +0200

    Create SecondCTA.vue

---
+++ src/components/SecondCTA.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="SecondCTA">
+  <p><slot>Second CTA</slot></p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SecondCTA',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.SecondCTA {
+  height: 48px;
+  color: #1F2933;
+  border:1px solid #1F2933;
+  border-radius: 4px;
+  background-color: rgba(255, 255, 255, 0);
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  padding: 0;
+}
+
+.SecondCTA p{
+  text-align: center;
+  margin: 12px 16px;
+  line-height: 24px;
+  align-self: middle;
+}
+</style>
