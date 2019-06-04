b62c0edf1ff38e1b0bd67c8ffdd48c326f14fc3e
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-04 15:26:11 +0200

    Create Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="menu">
+  <el-row type="flex"  class="menu-bg" justify="space-between" align="middle">
+      <el-col :sm="8" :md="8" :lg="7">
+        <img class="logo" src="../assets/logo.png">
+        <h4>Methodbox</h4>
+      </el-col>
+      <el-col :sm="6" :md="6" :lg="8"></el-col> <!-- For search bar -->
+        <el-col :sm="10" :md="10" :lg="9" class="alignItemsRight">
+            <slot name="MainCTA"></slot>
+             <SecondCTA class="alignedRight"><font-awesome-icon icon="user-circle" class="icon iconMarginRight"/>Test button</SecondCTA>
+            <thirdCTA class="alignedRight"><font-awesome-icon slot="Icon" icon="user-circle" class="icon iconMarginRight"/>Account</thirdCTA>
+        </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import MainCTA from './MainCTA.vue'
+import SecondCTA from './SecondCTA.vue'
+import thirdCTA from './thirdCTA.vue'
+
+export default {
+  name: 'Menu',
+  data () {
+    return {
+
+    }
+  },
+  components: {MainCTA, SecondCTA, thirdCTA}
+}
+
+</script>
+<style >
+.menu{
+    margin-bottom: 24px
+}
+</style>
+
