9f8a4f64bc600f01ad2f3a56edfa1431d2037b7e
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-04 15:26:04 +0200

    Create thirdCTA.vue

---
+++ src/components/thirdCTA.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="thirdCTA">
+  <slot name="Icon"></slot>
+  <p><slot></slot></p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'thirdCTA',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+button{
+  border:none;
+}
+.thirdCTA{
+  height: 48px;
+  color: #1F2933;
+  background-color: none;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  padding: 0;
+  border-color: none;
+}
+
+.thirdCTA p{
+  text-align: center;
+  margin: 0px;
+  line-height: 24px;
+  align-self: middle;
+  border-bottom: 1px solid #1F2933;
+}
+</style>
