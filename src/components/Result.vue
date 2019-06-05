18c1d23d8ee0fd7e27304ecff61af067a445db68
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-05 20:05:53 +0200

    Create Result.vue

---
+++ src/components/Result.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Result">
+    <slot name="Icon2"></slot>
+    <div><slot name="Key"></slot></div>
+    <div class="Value"><slot name="Value"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.Result{
+    padding:8px 0px;
+    display: flex;
+}
+.Value{
+    margin-left: auto;
+    font-weight: bold;
+}
+
+</style>
