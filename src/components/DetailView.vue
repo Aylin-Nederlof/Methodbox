386eb34a1de4a5a126e5bad51b03a9d75baac85e
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-05 20:06:01 +0200

    Create DetailView.vue

---
+++ src/components/DetailView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="wrapper">
+        <Menu>
+            <SecondCTA slot="SecondCTA" class="alignedRight"><font-awesome-icon icon="plus" class="icon iconMarginRight"/>Methode toevoegen</SecondCTA>
+        </Menu>
+        <h1>{{methode.title}}yeeeesy</h1>
+    </div>
+</template>
+
+<script>
+import Menu from './menu.vue'
+import MainCTA from './MainCTA.vue'
+import SecondCTA from './SecondCTA.vue'
+import thirdCTA from './thirdCTA.vue'
+import doelgroepTag from './doelgroepTag.vue'
+import Result from './Result.vue'
+
+
+    export default {
+        name: 'DetailView',
+        props: ['methode'],
+        data () {
+            return {
+                msg: 'bla'
+            }
+        },
+        components: {MainCTA, SecondCTA, thirdCTA, Menu, doelgroepTag, Result}
+    }
+</script>
+
+<style scoped>
+    h1, h2 {
+        font-weight: normal;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
