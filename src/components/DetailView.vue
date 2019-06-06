24d95dcb7f578113e1151fa6b71b0334305f790d
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-06 22:39:21 +0200

    Add klantresultaten en sorteren

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -19,11 +19,12 @@
         <div class="main">
             <el-row :gutter="24">
                 <el-col :sm="16" :md="16">
+                    <el-row>
                     <div class="method card-bg">
                         <el-row>
                         <el-col :span="20">
                             <div class="categoryTitle">
-                                <div class="categorie" ></div>
+                                <div class="categorie" v-bind:style="{ backgroundColor: categorieKleur }"></div>
                                 <div>
                                 <h2>{{methode.title}}</h2>
                                 <div class="subTitle">{{methode.category}} - {{methode.subCategory}}</div>
@@ -38,9 +39,28 @@
                     </div>
                     </el-row>
                     </div>
+                    </el-row>
+                    <el-row>
+                        <div class="klantresultaten">
+                        <el-col :span="12"><label> Klant resultaten ({{methode.timesUsed}}):</label></el-col>
+                        
+                        <el-col :span="12" class="alignItemsRight">
+                            <select v-model="currentOrder" class="alignedRight sorteer">
+                                <option value="naam">Sorteer op naam</option>
+                                <option value="kosten">Sorteer op kosten</option>
+                                <option value="ROI">Sorteer op ROI</option>
+                            </select>
+                        </el-col>
+                        </div>
+                     </el-row>                    
+                    <el-row v-for="klantResultaat in OrderedResults" :key="klantResultaat.naam">
+                        <klantResultaat :klantResultaatData="klantResultaat"></klantResultaat>
+                    </el-row>
+                    <!-- <p v-for="klantResultaat in orderedUsers" :key="klantResultaat.naam">test</p> -->
+
                 </el-col>
                 <el-col :sm="8" :md="8">
-                    <div class="resultaten">
+                    <div class="resultaten method">
                         <label class="padding">Gemiddelde van {{methode.timesUsed}} klant resultaten:</label>
                         <el-row>
                         <el-col :xs="24" :sm="24" :md="24" :lg="24">
@@ -107,6 +127,8 @@ import SecondCTA from './SecondCTA.vue'
 import thirdCTA from './thirdCTA.vue'
 import doelgroepTag from './doelgroepTag.vue'
 import Result from './Result.vue'
+import KlantResultaat from './KlantResultaat.vue'
+import _ from 'lodash'
 
 
     export default {
@@ -114,14 +136,121 @@ import Result from './Result.vue'
         props: ['methode'],
         data () {
             return {
-                msg: 'bla'
+                msg: 'bla',
+                currentOrder:'naam'
             }
         },
-        components: {MainCTA, SecondCTA, thirdCTA, Menu, doelgroepTag, Result}
+        methods: {
+        toggleOrder(currentOrder) {
+            this.currentOrder = currentOrder;
+        }
+         },
+    computed: {
+    OrderedResults: function () {
+        
+        console.log(this.currentOrder)
+        if(this.currentOrder === 'naam'){
+            return _.sortBy(this.methode.klantresultaten, this.currentOrder)
+        }
+        else{
+            return _.orderBy(this.methode.klantresultaten, [this.currentOrder], ['desc'])
+        }
+    },
+    categorieKleur: function () {
+      var kleur = ''
+      if (this.methode.category === 'focus') {
+        kleur = 'rgba(218,0,255,1)'
+      } else if (this.methode.category === 'visibility') {
+        kleur = 'rgba(255,0,91,1)'
+      } else if (this.methode.category === 'transfer') {
+        kleur = 'rgba(0,176,255,1)'
+      } else if (this.methode.category === 'fullfil') {
+        kleur = 'rgba(255,213,0,1)'
+      } else {
+        kleur = 'deze'
+      }
+      return kleur
+    },
+    gemOpbrengsten: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].Opbrengsten
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(2))
+    },
+    gemROI: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].ROI
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(0))
+    },
+    gemImplementatietijd: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].implementatietijd
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(0))
+    },
+    gemDoorlooptijd: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].doorlooptijd
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(0))
+    },
+    gemKosten: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].kosten
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(2))
+    },
+    gemMarge: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].marge
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(2))
+    },
+    gemConversieRatio: function () {
+      let sum = 0
+      let aantalKlanten = 0
+      for (let i = 0; i < this.methode.klantresultaten.length; i++) {
+      sum += this.methode.klantresultaten[i].conversieRatio
+      aantalKlanten++
+      }
+      var avg = sum / aantalKlanten
+      return parseFloat(avg.toFixed(2))
+    }
+    },
+        components: {MainCTA, SecondCTA, thirdCTA, Menu, doelgroepTag, Result, KlantResultaat}
     }
 </script>
 
 <style scoped>
+    .section{
+        margin: 0 0 40px;
+    }
+
    .resultaten{
        border-radius: 4px;
        padding: 24px 0;
@@ -136,5 +265,13 @@ import Result from './Result.vue'
        padding: 8px 24px;
    }
    
+   .klantresultaten{
+       display: flex;
+       margin: 16px 0 40px;
+   }
+
+   .KlantResultaat{
+        display: flex;
+   }
 
 </style>
