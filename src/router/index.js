665463d581beac9d8137f0a64b0b3b96f8e45061
aylinnederlof <a.nederlof@student.fontys.nl>
2019-06-07 15:17:47 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ export default new Router({
       component: Home
     },
     {
-      path: 'DetailView/:methodeNaam',
+      path: 'DetailView/:methodeID/:methodeNaam',
       name: 'DetailView',
       component: DetailView,
       props: true,
